88ad2e71b2a0190e7054c2bc01ed885aa18db62e
Ian Dao <ibdao921@gmail.com>
2022-06-28 11:53:58 -0700

    done with step1

---
+++ step1.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const fsP = require("fs/promises");
+
+async function cat(path){
+    try {
+        let contents = await fsP.readFile(path, "utf8");
+        console.log(contents);
+      } catch (err) {
+        console.log(err)
+        process.exit(1);
+      }
+}
+
+cat(process.argv[2]);
